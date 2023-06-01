9614f7460514e1936ea2972c8c0868ff60cbc168
prerana1821 <prerananawar1@gmail.com>
2023-06-02 03:29:10 +0530

    feat: using component composition

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import ReactDOM from "react-dom/client";
   * JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLELement(render)
 */
 
-const jsxHeading = (
+const JSXHeading = () => (
 	<h1 id='heading' className='heading' style={ { color: "white" } }>
 		This is a JSX heading
 	</h1>
@@ -15,11 +15,17 @@ const jsxHeading = (
 
 const HeadingComponent = () => <h1>This is a component heading.</h1>
 
+// component composition
 const HeadingComponent1 = () => {
-	return <h1>This is a component heading.</h1>
+	return (
+		<div>
+			<JSXHeading />
+			<h1>This is a component heading.</h1>
+		</div>
+	);
 }
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<HeadingComponent1 />);
