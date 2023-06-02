6d09457a739cb5f6e55d88809444cd4bd1bb15bc
prerana1821 <prerananawar1@gmail.com>
2023-06-03 03:16:23 +0530

    feat: different ways to call components in JSX

--- App.js
+++ App.js
@@ -7,6 +7,12 @@ import ReactDOM from "react-dom/client";
   * JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLELement(render)
 */
 
+const headingElement = (
+	<h1 id='heading' className='heading' style={ { color: "white" } }>
+		This is a JSX heading
+	</h1>
+);
+
 const JSXHeading = () => (
 	<h1 id='heading' className='heading' style={ { color: "white" } }>
 		This is a JSX heading
@@ -19,7 +25,10 @@ const HeadingComponent = () => <h1>This is a component heading.</h1>
 const HeadingComponent1 = () => {
 	return (
 		<div>
+			{ JSXHeading() }
 			<JSXHeading />
+			<JSXHeading></JSXHeading>
+			{/* { headingElement } - this is how you call element in JSX */ }
 			<h1>This is a component heading.</h1>
 		</div>
 	);
